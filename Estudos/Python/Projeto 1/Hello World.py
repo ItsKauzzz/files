4a0c1728b3f80bd1468b32970581c14de9d8f934
ItsKauzzz <kauzzresources@outlook.com>
2025-11-19 19:43:51 -0300

    Commit

---
+++ Estudos/Python/Projeto 1/Hello World.py
@@ -0,0 +1,10 @@
+print("Hello World")
+age = 30
+names = ["John", "Maria"]
+alturas = [1.32, 1.8]
+
+age, name, altura = 30, "Pedro", 1.7
+
+_input = input("Mensagem para o user")
+
+print("Voce digitou ${_input}")
---
+++ Estudos/Python/Projeto 1/sketchCode.py
@@ -0,0 +1,3 @@
+width = 30
+Height = 5
+area = 0
